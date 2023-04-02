7ff3a990dc81f5918df8ba3a9ed4425ad6d08720
Jotirling <swamijotirlingj@gmail.com>
2023-04-03 01:28:47 +0530

    Hello World Code

--- basic-programming/basic-programming.cpp
+++ basic-programming/basic-programming.cpp
@@ -6,3 +6,11 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World!!!" << endl;
+
+    return0;
+}
